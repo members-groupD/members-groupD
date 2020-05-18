9e6820e8a7449ec4157ee780b6b2fb6824dcd6fd
EC2 Default User <ec2-user@ip-172-31-26-183.us-east-2.compute.internal>
2020-05-18 06:14:36 +0000

    全体環境

---
+++ app/Http/Controllers/AnswersController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Question;
+use App\Answer;
+use APP\Cate;
+use Auth;
+use Validator;
+
+
+class AnswersController extends Controller
+{
+  
+}
