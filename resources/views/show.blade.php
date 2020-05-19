64777692cb24222f388d40932c4e62ad7bb00509
EC2 Default User <amoryanyan@gmail.com>
2020-05-19 06:30:50 +0000

    uratsuji branch

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,15 +1,36 @@
+@extends('layouts.app')
+
 <p>名前:{{$user->name}}</p>
 <p>メール:{{$user->email}}</p>
 
 @foreach($questions as $qestion)
     <p>投稿した質問:{{$qestion->title}}</p>
-    <a href="{{}}">編集</a>
-    <a href="{{}}">削除</a>
+    <form action="" method="POST">
+    {{csrf_field()}}
+        <button type="submit" class="btn btn-default">
+            編集
+        </button> 
+    </form>
+    <form action="" method="POST">
+    {{csrf_field()}}
+        <button type="submit" class="btn btn-default">
+            削除
+        </button> 
+    </form>
 @endforeach
 
-
 @foreach($answers as $answer)
-    <p>投稿した回答:</p>
-    <a href="{{}}">編集</a>
-    <a href="{{}}">削除</a>
+    <p>投稿した回答:{{$answer->content}}</p>
+    <form action="" method="POST">
+    {{csrf_field()}}
+        <button type="submit" class="btn btn-default">
+            編集
+        </button> 
+    </form>
+    <form action="" method="POST">
+    {{csrf_field()}}
+        <button type="submit" class="btn btn-default">
+            削除
+        </button> 
+    </form>
 @endforeach
