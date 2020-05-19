395cc9b82883dea46661c8fc5c82c75b88e2dfb7
EC2 Default User <amoryanyan@gmail.com>
2020-05-19 06:44:38 +0000

    HIRATA修正

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -11,11 +11,9 @@ use Validator;
 
 class QuestionsController extends Controller
 {
- hirata
     //コンストラクタ （このクラスが呼ばれると最初にこの処理をする）
     public function __construct()
 
- master
     {
         // ログインしていなかったらログインページに遷移する（この処理を消すとログインしなくてもページを表示する）
         $this->middleware('auth');
@@ -28,7 +26,6 @@ class QuestionsController extends Controller
         // テンプレート「questions/create.blade.php」を表示します。
     }
     
- hirata
     // ===ここまでリストを新規作成する処理の追加（フォームへの遷移）===
 
     //===ここから質問の一覧表示をするための処理追加===
@@ -51,7 +48,6 @@ class QuestionsController extends Controller
        $questions = Question::orderBy('created_at', 'asc')->get();
        $answers=Answer::orderBy('created_at', 'asc')->get();
         return view('questions/show', ['questions' => $questions],['answers' => $answers]);
- master
     }
     
 }
